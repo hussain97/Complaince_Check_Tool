77cae5d752e743c4146f556ee3bde2abeb77af8f
Hussain Moftah <hussainmoftah@sphonic.com>
2022-02-23 00:23:08 +0000

    HTML CIK Extraction
    Created web scrapping logic to extract the CIK code from the SEC gov website

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 import requests
-
+from bs4 import BeautifulSoup
 
 payload = {'company': 'Tesla'}
 r = requests.get('https://www.sec.gov/cgi-bin/cik_lookup', params=payload)
-print(r.url)
-print(r.text)
+r_Contents = r.text
+# Making a request to the SEC gov website to retrieve the CIK codes for the searched company
+
+soup = BeautifulSoup(r_Contents, 'lxml')
+Company_CIK_Code_HTML = soup.find('a')
+Company_CIK_Code = Company_CIK_Code_HTML.text
+print(Company_CIK_Code)
+# Extracting the first CIK code from the HTML file
