c20559f68e0cf5afae8108ef2dc5fbf75e45e842
Hussain Moftah <hussain_moftah@yahoo.co.uk>
2022-02-23 00:46:16 +0000

    Testing ATOM GitHub connection

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 
-payload = {'company': 'Tesla'}
+payload = {'company': 'MICROSOFT CORP'}
 r = requests.get('https://www.sec.gov/cgi-bin/cik_lookup', params=payload)
 r_Contents = r.text
 # Making a request to the SEC gov website to retrieve the CIK codes for the searched company
